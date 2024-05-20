ae3006f94b4ff0199929f66d70e9c4a30938fe36
huijia-yee <137977151+huijia-yee@users.noreply.github.com>
2024-05-20 16:42:37 +0800

    Add initial files

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from sentence_transformers import SentenceTransformer
+
+app = FastAPI()
+
+
+@app.get("/")
+def check_root():
+    return {"Hello": "World"}
+
+
+@app.get("/get_embed")
+def get_embedding(text):
+    model = SentenceTransformer(
+        'sentence-transformers/paraphrase-multilingual-mpnet-base-v2')
+    text = text.replace("\n", " ")
+    embeddings = model.encode(text).tolist()
+    return embeddings
